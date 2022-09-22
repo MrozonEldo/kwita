29dd8f0a6195168d2dcd14511386237d6fe1ba53
Mrożon <alanmrozik25@gmail.com>
2022-09-22 14:36:13 +0200

    nie wytrzymie

--- web/src/lib/ExpenseCard.svelte
+++ web/src/lib/ExpenseCard.svelte
@@ -3,10 +3,17 @@
     import { formatMoney } from '../helpers';
     export let expense;
 
+    function Delete(id){
+        const valueToRemove = Object.values(id);
+        console.log(valueToRemove[0]);
+        expense=expense.filter(item => item.id !== valueToRemove[0]);
+        console.log(expense);
+    }
 </script>
 
 <div class="expense_card">
     <p>Hej {expense.payer.name}! Chcę ci powiedzieć, że zapłaciłeś {formatMoney(expense.amount)} za {expense.title}. Spoko ok?</p>
+    <input type="button" value="USUŃ" on:click={()=>Delete(expense)}>
 </div>
 
 <style>
