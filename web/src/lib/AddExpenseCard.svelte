177abd2a9cc7fe2c7900acd285b2383f1e0a6bc9
Mrożon <alanmrozik25@gmail.com>
2022-09-24 22:52:46 +0200

    zalążki

--- web/src/lib/AddExpenseCard.svelte
+++ web/src/lib/AddExpenseCard.svelte
@@ -7,13 +7,14 @@
     let amountDecimal: number;
     let title: string;
     let category: ExpenseCategory;
+    let payer = $user;
 
     function Add(){
         let newExpense: Expense = {
             id: crypto.randomUUID(),
             date: new Date(),
             amount: amountDecimal*100,
-            payer: $user,
+            payer: payer,
             title: title
         }
 
@@ -24,6 +25,12 @@
 
 <div class="box"> 
     <form on:submit|preventDefault={Add}>
+        Kto:
+        <select class="users" bind:value={payer} required>
+            {#each $users as u}
+                <option value={u}>{u.name}</option>
+            {/each}
+        </select>
         Ile:
         <input
             type="number"
---
+++ web/src/lib/UserList.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+    import {users} from '../context'
+    import { expenses } from '../context';
+
+</script>
+
+        {#each $users as u}
+            <div>
+                {u.name}:
+                <br>
+                Liczba rachunków:0
+                <br>
+                Suma:
+            </div>
+        {/each}
+
+<style>
+    div{
+        border:solid 1px black;
+        margin:10px;
+        text-align: left;
+    }
+</style>
